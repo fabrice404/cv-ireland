61bd6ffa0c9f4b615b69a75a30efea685a4ee705
fabrice404 <fabrice.lamant@ecolutis.com>
2017-11-13 17:02:42 +0100

    WIP styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,8 +9,8 @@ class App extends Component {
           <h1 className="me--name">{this.props.cv.firstName} {this.props.cv.lastName}</h1>
           <h2 className="me--jobTitle">{this.props.cv.jobTitle}</h2>
         </header>
-        <section>
-          <h3>Summary</h3>
+        <section className="section summary">
+          <h3 className="section--title">Summary</h3>
           {this.props.cv.summary}
         </section>
         <Experience experience={this.props.cv.experience}/>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,36 +5,41 @@ import ListItem from './ListItem'
 class Experience extends Component {
   render() {
     return (
-      <div>
-        <h3>Experience</h3>
+      <section className="section experience">
+        <h3 class="section--title">Experience</h3>
         {this.props.experience.map(item => <ExperienceItem {...item} key={Math.random(0, 1)}/>)}
-      </div>
+      </section>
     );
   }
 }
 
 const ExperienceItem = props => (
-  <div className="job">
-    <span className="job--title">{props.jobTitle}</span>
-    <span className="job--comany">{props.company}</span>
-    {props.link && <span className="job--link"><a href={props.link} target="_blank">{props.link}</a></span>}
-    <div>
-      <span>{moment(props.startDate.month + ' ' + props.startDate.year, 'MM YYYY').format('MMM YYYY')}</span>
-      <span>
+  <div>
+    <span className="title-1">{props.jobTitle}</span>
+    <span className="title-2 company">{props.company}</span>
+
+    <span className="date">
+      <span className="date-start">{moment(props.startDate.month + ' ' + props.startDate.year, 'MM YYYY').format('MMM YYYY')}</span>
+      <span className="date-end">
         {props.endDate && moment(props.endDate.month + ' ' + props.endDate.year, 'MM YYYY').format('MMM YYYY')}
         {!props.endDate && 'present'}
       </span>
-    </div>
-    {props.description}
+    </span>
+
+    {props.link && <span className="link">
+      <a href={props.link} target="_blank">{props.link}</a>
+    </span>}
+
+    {props.description&& <div>{props.description}</div>}
     {props.projects && props.projects.map(item => <Project {...item} key={Math.random(0, 1)}/>)}
   </div>
 );
 
 const Project = props => (
-  <div>
-    {props.title}
-    {props.teamSize}
-    {props.role}
+  <div className="project">
+    {props.title && <div className="title">Project: {props.title}</div>}
+    {props.teamSize && <div className="title">Team size: {props.teamSize}</div>}
+    {props.role && <div className="title">Role: {props.role}</div>}
     {props.description}
     {props.responsibilities && <ul>{props.responsibilities.map(item => <ListItem item={item} key={Math.random(0, 1)}/>)}</ul>}
     {props.environment && <ul>{props.environment.map(item => <ListItem item={item} key={Math.random(0, 1)}/>)}</ul>}
